1fc0e14ae2cb9c8e19552ad1e7982f526e51e667
Fnk7 <34437956+Fnk7@users.noreply.github.com>
2021-09-17 17:01:27 +0800

    Update workload.h

--- include/workload.h
+++ include/workload.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <cstddef>
+
 class Workload
 {
 public:
-    void* align_malloc(size_t size, size_t alignment);
-    void align_free();
+    void* aligned_malloc(size_t size, size_t alignment);
+    void aligned_free(void *p);
     virtual void init() = 0;
     virtual void run() = 0;
     virtual bool check() = 0;
