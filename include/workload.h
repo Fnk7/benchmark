eb1b5a62cb648e80c1e3f51d9f8ca6c7d7ddc7bb
Fnk7 <34437956+Fnk7@users.noreply.github.com>
2021-09-17 15:29:13 +0800

    Create workload.h

---
+++ include/workload.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Workload
+{
+public:
+    Workload() = delete;
+    void* align_malloc(size_t size, size_t alignment);
+    void align_free();
+    virtual void init() = delete;
+    virtual void run() = delete;
+    virtual void check() = delete();
+    virtual void release() = delete();
+    void evaluate();
+}
