ad63b8a9ac840f22a488ebe87e779afab87433f9
Fnk7 <34437956+Fnk7@users.noreply.github.com>
2021-09-17 16:24:51 +0800

    Update workload.h

--- include/workload.h
+++ include/workload.h
@@ -3,13 +3,12 @@
 class Workload
 {
 public:
-    Workload() = delete;
     void* align_malloc(size_t size, size_t alignment);
     void align_free();
-    virtual void init() = delete;
-    virtual void run() = delete;
-    virtual bool check() = delete();
-    virtual void release() = delete();
+    virtual void init() = 0;
+    virtual void run() = 0;
+    virtual bool check() = 0;
+    virtual void release() = 0;
     void evaluate();
 }
 
