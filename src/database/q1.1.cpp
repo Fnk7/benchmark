886e95149eba2ac7358d1cc77d84e8a63a519e70
Fnk7 <34437956+Fnk7@users.noreply.github.com>
2021-09-22 17:24:30 +0800

    Update q1.1.cpp

--- src/database/q1.1.cpp
+++ src/database/q1.1.cpp
@@ -18,7 +18,18 @@ public:
     void init()
     {
         Database::init();
-        // TODO
+        lineorder_lo_orderdate = (uint32_t *)aligned_malloc(lineorder_size * sizeof(uint32_t), 8);
+        lineorder_lo_quantity = (uint8_t *)aligned_malloc(lineorder_size * sizeof(uint8_t), 8);
+        lineorder_lo_extendedprice = (uint64_t *)aligned_malloc(lineorder_size * sizeof(uint64_t), 8);
+        lineorder_lo_discount = (uint8_t *)aligned_malloc(lineorder_size * sizeof(uint8_t), 8);
+        date_d_datekey = (uint32_t *)aligned_malloc(lineorder_size * sizeof(uint32_t), 8);
+        date_d_year = (uint16_t *)aligned_malloc(lineorder_size * sizeof(uint16_t), 8);
+        load_bin(datapath + "lineorder/LO_ORDERDATE.bin", lineorder_lo_orderdate, lineorder_size * sizeof(uint32_t));
+        load_bin(datapath + "lineorder/LO_QUANTITY.bin", lineorder_lo_quantity, lineorder_size * sizeof(uint8_t));
+        load_bin(datapath + "lineorder/LO_EXTENDEDPRICE.bin", lineorder_lo_extendedprice, lineorder_size * sizeof(uint64_t));
+        load_bin(datapath + "lineorder/LO_DISCOUNT.bin", lineorder_lo_discount, lineorder_size * sizeof(uint8_t));
+        load_bin(datapath + "date/D_DATEKEY.bin", date_d_datekey, lineorder_size * sizeof(uint32_t));
+        load_bin(datapath + "date/D_YEAR.bin", date_d_year, lineorder_size * sizeof(uint16_t));
     }
     
     void run()
@@ -29,11 +40,17 @@ public:
     bool check()
     {
         // TODO
+        return true;
     }
     
     void release()
     {
-        // TODO
+        aligned_free(lineorder_lo_orderdate);
+        aligned_free(lineorder_lo_quantity);
+        aligned_free(lineorder_lo_extendedprice);
+        aligned_free(lineorder_lo_discount);
+        aligned_free(date_d_datekey);
+        aligned_free(date_d_year);
     }
 };
 
